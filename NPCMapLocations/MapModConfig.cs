2548ed9334c719a8af6b12dcaa3eb0103355b3c4
Bouhm <bbouhm@gmail.com>
2018-03-31 02:28:07 +0100

    one more tweak

--- NPCMapLocations/MapModConfig.cs
+++ NPCMapLocations/MapModConfig.cs
@@ -47,7 +47,7 @@ namespace NPCMapLocations
         public bool showVincent { get; set; } = true;
         public bool showWilly { get; set; } = true;
         public bool showSandy { get; set; } = false;
-        public bool showWizard { get; set; } = false;
+        public bool showWizard { get; set; } = true;
         public bool showMarlon { get; set; } = false;
         public bool showTravelingMerchant { get; set; } = true;
         public Dictionary<string, int> villagerCrop { get; set; } = new Dictionary<string, int>();
