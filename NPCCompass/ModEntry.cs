95a659728f2012b99b2cba9dcc37e8ebb7781d38
Bouhm <bbouhm@gmail.com>
2018-04-25 19:18:49 -0700

    trying to set origin to player's position but math is hard

--- NPCCompass/ModEntry.cs
+++ NPCCompass/ModEntry.cs
@@ -14,18 +14,17 @@ namespace NPCCompass
     {
         private IModHelper helper;
         public static IMonitor monitor;
-        private ModData constants;
-        private HashSet<Locator> locators;
-        private const int SCALE = 4;
+        private static ModData constants;
+        private static HashSet<Locator> locators;
 
+        // For debug info
         private static double _angle;
         private static Vector2 _viewportPos;
         private static Vector2 _npcPos;
         private static Vector2 _playerPos;
         private static Vector2 _locatorPos;
 
-        // For debug info
-        private const bool DEBUG_MODE = false;
+        private const bool DEBUG_MODE = true;
 
         public override void Entry(IModHelper helper)
         {
@@ -47,15 +46,27 @@ namespace NPCCompass
         // 0 rad will be at line from center of viewport to (0, viewportHeight/2) relative to viewport
         private double GetPlayerToNPCAngle(Vector2 playerPos, Vector2 npcPos)
         {
-            // Triangle such that hypotenuse is the line from player to npc
+            // Hypotenuse is the line from player to npc
             float opposite = npcPos.Y - playerPos.Y;
             float adjacent = npcPos.X - playerPos.X;
             double angle = Math.Atan(opposite / adjacent);
 
-            return angle += MathHelper.Pi;
+            if (opposite > 0 && adjacent > 0 || opposite < 0 && adjacent > 0)
+                angle += MathHelper.Pi;
+            else if (opposite > 0 && adjacent < 0)
+                angle += MathHelper.TwoPi;
+
+            return angle;
         }
 
         // Get quadrant to draw the locator on based on the angle
+        //   _________
+        //  | \  2  / |
+        //  |   \ /   |
+        //  | 1  |  3 |
+        //  |   / \   |
+        //  | /__4__\_|
+        //
         private int GetViewportQuadrant(double angle)
         {
             float length = Game1.viewport.Height / 2; // Game1.player.position.X - Game1.viewport.X;
@@ -89,8 +100,8 @@ namespace NPCCompass
         // the viewport quadrant and positions of player/npc relative to map
         private Vector2 GetLocatorPosition(double angle, int quadrant)
         {
-            float x = 0;
-            float y = 0;
+            float x;
+            float y;
 
             // Draw triangle such that the hypotenuse is
             // the line from player to the point of intersection of
@@ -98,17 +109,17 @@ namespace NPCCompass
             switch (quadrant)
             {
                 case 1:
-                    y += Game1.viewport.Height/2 - (Game1.viewport.Width/2 * (float)Math.Tan(angle));
+                    y = Game1.viewport.Height/2 - (Game1.viewport.Width/2 * (float)Math.Tan(angle));
                     return new Vector2(0, y);
                 case 2:
-                    x += Game1.viewport.Width/2 - (Game1.viewport.Height/2 * (float)Math.Tan(MathHelper.PiOver2 - angle));
+                    x = Game1.viewport.Width/2 - (Game1.viewport.Height/2 * (float)Math.Tan(MathHelper.PiOver2 - angle));
                     return new Vector2(x, 0);
                 case 3:
-                    y += Game1.viewport.Height/2 - (Game1.viewport.Width/2 * (float)Math.Tan(MathHelper.Pi - angle));
-                    return new Vector2(Game1.viewport.Width - Game1.tileSize, y);
+                    y = Game1.viewport.Height/2 - (Game1.viewport.Width/2 * (float)Math.Tan(MathHelper.Pi - angle));
+                    return new Vector2(Game1.viewport.Width, y);
                 case 4:
-                    x += Game1.viewport.Width/2 - (Game1.viewport.Height/2 * (float)Math.Tan(3 * MathHelper.PiOver2 - angle));
-                    return new Vector2(x, Game1.viewport.Height - Game1.tileSize);
+                    x = Game1.viewport.Width/2 - (Game1.viewport.Height/2 * (float)Math.Tan(3 * MathHelper.PiOver2 - angle));
+                    return new Vector2(x, Game1.viewport.Height);
                 default:
                     return Vector2.Zero;
             }
@@ -119,13 +130,18 @@ namespace NPCCompass
             locators = new HashSet<Locator>();
             foreach (NPC npc in Utility.getAllCharacters())
             {
-                if ((npc.Schedule == null && !npc.isMarried()) || npc.currentLocation == null) { continue; }
-                //monitor.Log(npc.currentLocation.name);
-                if (constants.Location[npc.currentLocation.name] == null || !constants.Location[npc.currentLocation.name].Equals(Game1.player.currentLocation.name)) { continue; }
-                if (Utility.isOnScreen(new Vector2(npc.position.X, npc.position.Y), 2 * Game1.tileSize)) { continue; }
+                if ((npc.Schedule == null 
+                    && !npc.isMarried()) 
+                    || npc.currentLocation == null 
+                    || !constants.Location.ContainsKey(npc.currentLocation.name)
+                   ) { continue; }
+                if (constants.Location[npc.currentLocation.name] == null 
+                    || !constants.Location[npc.currentLocation.name].Equals(Game1.player.currentLocation.name)
+                   ) { continue; }
+                //if (Utility.isOnScreen(new Vector2(npc.position.X, npc.position.Y), Game1.tileSize)) { continue; }
 
-                Vector2 playerPos = new Vector2(Game1.player.position.X + Game1.pixelZoom * Game1.player.sprite.spriteWidth/2, Game1.player.position.Y);
-                Vector2 npcPos = new Vector2(npc.position.X + Game1.pixelZoom * npc.sprite.spriteWidth/2, npc.position.Y);
+                Vector2 playerPos = new Vector2(Game1.player.position.X + Game1.pixelZoom * Game1.player.sprite.spriteWidth / 2, Game1.player.position.Y);
+                Vector2 npcPos = new Vector2(npc.position.X + Game1.pixelZoom * npc.sprite.spriteWidth / 2, npc.position.Y);
                 Locator locator = new Locator
                 {
                     Name = npc.name,
@@ -133,21 +149,22 @@ namespace NPCCompass
                     Proximity = GetDistance(playerPos, npcPos)
                 };
 
-                double angle = GetPlayerToNPCAngle(new Vector2(Game1.viewport.X + Game1.viewport.Width/2, Game1.viewport.Y + Game1.viewport.Height/2), npcPos); // playerPos, npcPos);
+                double angle = GetPlayerToNPCAngle(playerPos, npcPos);
                 int quadrant = GetViewportQuadrant(angle);
                 Vector2 locatorPos = GetLocatorPosition(angle, quadrant);
-
-                ModEntry._angle = angle;
-                ModEntry._viewportPos = new Vector2(Game1.viewport.X, Game1.viewport.Y);
-                ModEntry._playerPos = playerPos;
-                ModEntry._npcPos = npcPos;
-                ModEntry._locatorPos = locatorPos;
-
 
                 locator.X = locatorPos.X; 
                 locator.Y = locatorPos.Y; 
                 locators.Add(locator);
-                
+
+                if (DEBUG_MODE && locators.Count == 1)
+                {
+                    ModEntry._angle = angle;
+                    ModEntry._viewportPos = new Vector2(Game1.viewport.X, Game1.viewport.Y);
+                    ModEntry._playerPos = playerPos;
+                    ModEntry._npcPos = npcPos;
+                    ModEntry._locatorPos = locatorPos;
+                }
             }
         }
 
@@ -156,7 +173,16 @@ namespace NPCCompass
             if (!Context.IsWorldReady) { return; }
             foreach (Locator locator in locators)
             {
-                Game1.spriteBatch.Draw(locator.Marker, new Vector2(locator.X - 32, locator.Y - 30), new Rectangle?(new Rectangle(0, constants.MarkerCrop[locator.Name], 16, 15)), Color.White, 0f, Vector2.Zero, Game1.pixelZoom, SpriteEffects.None, 1f);
+                Game1.spriteBatch.Draw(
+                    locator.Marker,
+                    new Vector2(locator.X - 32, locator.Y - 30), 
+                    new Rectangle?(new Rectangle(0, constants.MarkerCrop[locator.Name], 16, 15)), 
+                    Color.White, 0f, 
+                    Vector2.Zero, 
+                    Game1.pixelZoom, 
+                    SpriteEffects.None, 
+                    1f
+                );
             }
         }
 
@@ -176,12 +202,14 @@ namespace NPCCompass
             // Black background for legible text
             Game1.spriteBatch.Draw(Game1.shadowTexture, new Rectangle(0, 0, 500, 286), new Rectangle(6, 3, 1, 1), Color.Black);
 
-            DrawText("Angle (rad): " + Math.Round(ModEntry._angle, 2), new Vector2(Game1.tileSize / 4, Game1.tileSize / 4));
-            DrawText("Locator: (" + (int)ModEntry._locatorPos.X + ", " + (int)ModEntry._locatorPos.Y + ")", new Vector2(Game1.tileSize / 4, Game1.tileSize * 3 / 4 + 8));
-            DrawText("Player: (" + (int)ModEntry._playerPos.X + ", " + (int)ModEntry._playerPos.Y + ")", new Vector2(Game1.tileSize / 4, Game1.tileSize * 5 / 4 + 8 * 2));
-            DrawText("NPC: (" + (int)ModEntry._npcPos.X + ", " + (int)ModEntry._npcPos.Y + ")", new Vector2(Game1.tileSize / 4, Game1.tileSize * 7 / 4 + 8 * 3));
-            DrawText("Viewport: (" + (int)ModEntry._viewportPos.X + ", " + (int)ModEntry._viewportPos.Y + ")", new Vector2(Game1.tileSize / 4, Game1.tileSize * 9 / 4 + 8 * 4));
-            DrawText(Game1.viewport.Width + "x" + Game1.viewport.Height, new Vector2(Game1.tileSize / 4, Game1.tileSize * 11 / 4 + 8 * 5));
+            Color color = ModEntry.locators.Count == 0 ? Color.DimGray : Color.White;
+
+            DrawText("Angle (rad): " + Math.Round(ModEntry._angle, 2), new Vector2(Game1.tileSize / 4, Game1.tileSize / 4), color);
+            DrawText("Locator: (" + (int)ModEntry._locatorPos.X + ", " + (int)ModEntry._locatorPos.Y + ")", new Vector2(Game1.tileSize / 4, Game1.tileSize * 3 / 4 + 8), color);
+            DrawText("Player: (" + (int)ModEntry._playerPos.X + ", " + (int)ModEntry._playerPos.Y + ")", new Vector2(Game1.tileSize / 4, Game1.tileSize * 5 / 4 + 8 * 2), color);
+            DrawText("NPC: (" + (int)ModEntry._npcPos.X + ", " + (int)ModEntry._npcPos.Y + ")", new Vector2(Game1.tileSize / 4, Game1.tileSize * 7 / 4 + 8 * 3), color);
+            DrawText("Viewport: (" + (int)ModEntry._viewportPos.X + ", " + (int)ModEntry._viewportPos.Y + ")", new Vector2(Game1.tileSize / 4, Game1.tileSize * 9 / 4 + 8 * 4), color);
+            DrawText(Game1.viewport.Width + "x" + Game1.viewport.Height, new Vector2(Game1.tileSize / 4, Game1.tileSize * 11 / 4 + 8 * 5), color);
         }
 
         // Draw outlined text
@@ -196,20 +224,27 @@ namespace NPCCompass
 
         private void GraphicsEvents_OnPostRenderEvent(object sender, EventArgs e)
         {
-            if (!Context.IsWorldReady) { return; }
+            if (!DEBUG_MODE || !Context.IsWorldReady) { return; }
             foreach (NPC npc in Utility.getAllCharacters())
             {
-                if ((npc.Schedule == null && !npc.isMarried()) || npc.currentLocation == null) { continue; }
-                if (constants.Location[npc.currentLocation.name] == null || !constants.Location[npc.currentLocation.name].Equals(Game1.player.currentLocation.name)) { continue; }
+                if ((npc.Schedule == null
+                    && !npc.isMarried())
+                    || npc.currentLocation == null
+                    || !constants.Location.ContainsKey(npc.currentLocation.name)
+                   ) { continue; }
+                if (constants.Location[npc.currentLocation.name] == null
+                    || !constants.Location[npc.currentLocation.name].Equals(Game1.player.currentLocation.name)
+                   ) { continue; }
 
-                float viewportX = Game1.viewport.Width/2; // Game1.player.position.X - Game1.viewport.X;
-                float viewportY = Game1.viewport.Height/2; // Game1.player.position.Y - Game1.viewport.Y;
+                float viewportX = Game1.player.position.X + Game1.pixelZoom * Game1.player.sprite.spriteWidth/2 - Game1.viewport.X;
+                float viewportY = Game1.player.position.Y - Game1.viewport.Y;
                 float npcViewportX = npc.position.X + Game1.pixelZoom * npc.sprite.spriteWidth/2 - Game1.viewport.X;
                 float npcViewportY = npc.position.Y - Game1.viewport.Y;
                 // Draw NPC sprite noodle connecting center of screen to NPC for debugging
                 DrawLine(Game1.spriteBatch, new Vector2(viewportX, viewportY), new Vector2(npcViewportX, npcViewportY), npc.sprite.Texture);
             }
-            ShowDebugInfo();
+            if (locators.Count == 1)
+                ShowDebugInfo();
         }
     }
 
