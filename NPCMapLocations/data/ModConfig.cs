68b4536299ab194e44a8d30009d8a03ddf98c124
bbpak <brianbpak@gmail.com>
2021-02-14 10:22:36 -0800

    make detailed islands disabled by default

--- NPCMapLocations/data/ModConfig.cs
+++ NPCMapLocations/data/ModConfig.cs
@@ -18,7 +18,6 @@ namespace NPCMapLocations
 	  public bool ShowHiddenVillagers { get; set; } = false;
 	  public bool MarkQuests { get; set; } = true;
 	  public bool ShowTravelingMerchant { get; set; } = true;
-
     public bool ShowMinimap { get; set; } = false;
 	  public int MinimapX { get; set; } = 12;
 	  public int MinimapY { get; set; } = 12;
@@ -31,15 +30,14 @@ namespace NPCMapLocations
     public string MenuKey { get; set; } = "Tab";
     public string TooltipKey { get; set; } = "Space";
     public bool ShowFarmBuildings { get; set; } = true;
-
     public string MinimapToggleKey { get; set; } = "OemPipe";
     public HashSet<string> MinimapBlacklist { get; set; } = new HashSet<string>() { };
-
     public bool UseSeasonalMaps { get; set; } = true;
     public bool ShowChildren { get; set; } = false;
     public bool ShowHorse { get; set; } = true;
     public Dictionary<string, int> CustomNpcMarkerOffsets { get; set; } = new Dictionary<string, int>();
     public HashSet<string> NpcBlacklist { get; set; } = new HashSet<string>() { };
+    public bool UseDetailedIsland { get; set; } = true;
     public bool DEBUG_MODE { get; set; } = false;
   }
 }
