5dad2b7f9bdb9b7173ceb2c1741f6d63d889b3c8
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2025-05-24 23:58:56 -0400

    group settings in config model

--- NPCMapLocations/Framework/Models/ModConfig.cs
+++ NPCMapLocations/Framework/Models/ModConfig.cs
@@ -12,59 +12,29 @@ public class ModConfig
     /*********
     ** Accessors
     *********/
+    /****
+    ** Constants
+    ****/
     /// <summary>The maximum heart level that can be configured for <see cref="HeartLevelMin"/> or <see cref="HeartLevelMax"/>.</summary>
     internal const int MaxPossibleHeartLevel = 14;
+
+    /****
+    ** Controls
+    ****/
+    /// <summary>The key binding to toggle the floating minimap.</summary>
+    public string MinimapToggleKey { get; set; } = "OemPipe";
 
     /// <summary>The key binding to cycle the tooltip position when on the map view.</summary>
     public string TooltipKey { get; set; } = "Space";
 
-    /// <summary>The number of ticks between each minimap update.</summary>
-    public uint MiniMapCacheTicks { get; set; } = 15;
-
-    /// <summary>The number of ticks between each update to NPC markers.</summary>
-    public uint NpcCacheTicks { get; set; } = 30;
-
-    /// <summary>The tooltip position when pointing at something on the map.</summary>
-    public int NameTooltipMode { get; set; } = 1;
-
-    /// <summary>Whether to only show villagers in the same location as the player.</summary>
-    public bool OnlySameLocation { get; set; } = false;
-
-    /// <summary>Which NPCs to display.</summary>
-    public VillagerVisibility ImmersionOption { get; set; } = VillagerVisibility.All;
-
-    /// <summary>Whether to only show villagers whose hearts with the player is between <see cref="HeartLevelMin"/> and <see cref="HeartLevelMax"/>.</summary>
-    public bool ByHeartLevel { get; set; } = false;
-
-    /// <summary>The minimum heart level for <see cref="ByHeartLevel"/>.</summary>
-    public int HeartLevelMin { get; set; } = 0;
-
-    /// <summary>The maximum heart level for <see cref="ByHeartLevel"/>.</summary>
-    public int HeartLevelMax { get; set; } = MaxPossibleHeartLevel;
-
-    /// <summary>Whether to show villagers that would normally be hidden. </summary>
-    public bool ShowHiddenVillagers { get; set; } = false;
-
-    /// <summary>Whether to mark NPCs with quests or birthdays today.</summary>
-    public bool ShowQuests { get; set; } = true;
-
-    /// <summary>Whether to show the bookseller when he's in town.</summary>
-    public bool ShowBookseller { get; set; } = true;
-
-    /// <summary>Whether to show the Traveling Merchant when she's in the forest.</summary>
-    public bool ShowTravelingMerchant { get; set; } = true;
-
+    /****
+    ** Minimap
+    ****/
     /// <summary>Whether to show the floating minimap.</summary>
     public bool ShowMinimap { get; set; } = false;
 
     /// <summary>Whether to lock the minimap position, so the player can't right-click to drag it.</summary>
     public bool LockMinimapPosition { get; set; } = false;
-
-    /// <summary>Whether to show farm buildings on the map.</summary>
-    public bool ShowFarmBuildings { get; set; } = true;
-
-    /// <summary>The key binding to toggle the floating minimap.</summary>
-    public string MinimapToggleKey { get; set; } = "OemPipe";
 
     /// <summary>The minimap's pixel X position on screen.</summary>
     public int MinimapX { get; set; } = 12;
@@ -81,11 +51,11 @@ public class ModConfig
     /// <summary>Location names in which the minimap should be disabled.</summary>
     public HashSet<string> MinimapExclusions { get; set; } = new(StringComparer.OrdinalIgnoreCase);
 
-    /// <summary>Whether to show player children on the map.</summary>
-    public bool ShowChildren { get; set; } = false;
-
-    /// <summary>Whether to show horses on the map.</summary>
-    public bool ShowHorse { get; set; } = true;
+    /****
+    ** Map display
+    ****/
+    /// <summary>The tooltip position when pointing at something on the map.</summary>
+    public int NameTooltipMode { get; set; } = 1;
 
     /// <summary>A multiplier to apply to NPC marker sizes on the map, where 1 is the default size.</summary>
     public float NpcMarkerScale { get; set; } = 1f;
@@ -96,12 +66,63 @@ public class ModConfig
     /// <summary>A multiplier to apply to other players' marker sizes on the map, where 1 is the default size.</summary>
     public float OtherPlayerMarkerScale { get; set; } = 1f;
 
+    /****
+    ** Filters
+    ****/
+    /// <summary>Which NPCs to display.</summary>
+    public VillagerVisibility ImmersionOption { get; set; } = VillagerVisibility.All;
+
+    /// <summary>Whether to only show villagers in the same location as the player.</summary>
+    public bool OnlySameLocation { get; set; } = false;
+
+    /// <summary>Whether to only show villagers whose hearts with the player is between <see cref="HeartLevelMin"/> and <see cref="HeartLevelMax"/>.</summary>
+    public bool ByHeartLevel { get; set; } = false;
+
+    /// <summary>The minimum heart level for <see cref="ByHeartLevel"/>.</summary>
+    public int HeartLevelMin { get; set; } = 0;
+
+    /// <summary>The maximum heart level for <see cref="ByHeartLevel"/>.</summary>
+    public int HeartLevelMax { get; set; } = MaxPossibleHeartLevel;
+
+    /// <summary>Whether to mark NPCs with quests or birthdays today.</summary>
+    public bool ShowQuests { get; set; } = true;
+
+    /// <summary>Whether to show villagers that would normally be hidden. </summary>
+    public bool ShowHiddenVillagers { get; set; } = false;
+
+    /// <summary>Whether to show the bookseller when he's in town.</summary>
+    public bool ShowBookseller { get; set; } = true;
+
+    /// <summary>Whether to show the Traveling Merchant when she's in the forest.</summary>
+    public bool ShowTravelingMerchant { get; set; } = true;
+
+    /// <summary>Whether to show horses on the map.</summary>
+    public bool ShowHorse { get; set; } = true;
+
+    /// <summary>Whether to show player children on the map.</summary>
+    public bool ShowChildren { get; set; } = false;
+
+    /// <summary>Whether to show farm buildings on the map.</summary>
+    public bool ShowFarmBuildings { get; set; } = true;
+
+    /****
+    ** Advanced
+    ****/
     /// <summary>Override the visibility for specific NPCs.</summary>
     public Dictionary<string, bool> NpcVisibility { get; set; } = new(StringComparer.OrdinalIgnoreCase);
 
     /// <summary>Custom offsets when drawing vanilla NPCs.</summary>
     public Dictionary<string, int> NpcMarkerOffsets { get; set; } = new(StringComparer.OrdinalIgnoreCase);
 
+    /// <summary>The number of ticks between each minimap update.</summary>
+    public uint MiniMapCacheTicks { get; set; } = 15;
+
+    /// <summary>The number of ticks between each update to NPC markers.</summary>
+    public uint NpcCacheTicks { get; set; } = 30;
+
+    /****
+    ** Internal
+    ****/
     /// <summary>NPC names to hide from the map, specified by mods through the API.</summary>
     [JsonIgnore]
     public HashSet<string> ModNpcExclusions { get; } = new(StringComparer.OrdinalIgnoreCase);
